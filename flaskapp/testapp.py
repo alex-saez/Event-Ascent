a0d8e40e574059a6ba26b5cccd1626a916142147
alex-saez <as3171@columbia.edu>
2017-01-31 20:10:52 -0500

    Date input works

--- flaskapp/testapp.py
+++ flaskapp/testapp.py
@@ -15,47 +15,43 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route('/<int:mux>')
 def gindex():
     """
     When you request the gaus path, you'll get the gaus.html template.
 
     """
-    mux = request.args.get('mux', '')
-    muy = request.args.get('muy', '')
-    if len(mux)==0: mux="3."
-    if len(muy)==0: muy="3."
-    return render_template("pagelayout4.html",mux=mux,muy=muy)
+    
+    #pull date from input field
+    #mux = int(request.args.get('mux'))
+
+    mux = request.args.get('mux',6190)
+    return render_template("pagelayout4.html",mux=mux)
 
 
+@app.route('/gdata')
+@app.route('/gdata/<int:mux>')
+def gdata(mux=None):
+    
+    mux = int(request.args.get('mux'))
 
-@app.route("/gdata")
-@app.route("/gdata/<float:mux>/<float:muy>")
-def gdata(ndata=10,mux=.5,muy=0.5):
-    """
-    On request, this returns a list of ``ndata`` randomly made data points.
-    about the mean mux,muy
-
-    :param ndata: (optional)
-        The number of data points to return.
-
-    :returns data:
-        A JSON string of ``ndata`` data points.
-
-    """
-
-    #pull 'birth_month' from input field and store it
-    #patient = int(request.args.get('mux'))
     
     # find topics
-    topics = findTopics(5960)
+    topics = findTopics(mux)
     
     ntopics = len(topics['titles'])
     A = [len(i) for i in topics['titles']]
     x = np.arange(ntopics)
     y = [0]*ntopics
-    #col = np.linspace(0,1,ntopics)[np.random.permutation(ntopics)]
+
+
     col = ["#156b87", "#876315", "#543510", "#872815"]*2
-    t = ['topic{}'.format(i+1) for i in range(ntopics)]
+          
+         
+#    t = [[w+' ' for w in topics['summaries'][i]] for i in range(ntopics)]
+    kw = ['\n'.join(t) for t in topics['summaries']]
+    
+    summ = topics['titles']
 
 #    summs_titles = []
 #    for i,s in enumerate(topics['summaries']):
@@ -67,9 +63,14 @@ def gdata(ndata=10,mux=.5,muy=0.5):
 
 
 
-    return json.dumps([{"_id": i, "x": x[i], "y": y[i], "area": A[i], "color": col[i], "tname": t[i]}
-                        for i in range(ntopics)])
-    
+    return json.dumps([{"_id": i, 
+                        "x": x[i], 
+                        "y": y[i], 
+                        "area": A[i], 
+                        "color": col[i], 
+                        "keywords": kw[i],
+                        "summary": summ[i]} 
+                      for i in range(ntopics)])
     
 
 if __name__ == "__main__":
@@ -81,4 +82,5 @@ if __name__ == "__main__":
     os.system("open http://localhost:{0}/".format(port))
 
     # Set up the development server on port 8000.
-    app.run(port=port, debug = True)
+    app.debug = True
+    app.run(port=port)
