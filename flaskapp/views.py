a285cd0789bc366126677c081c56d61426ca6ef9
Alex Saez <as3171@columbia.edu>
2017-02-09 15:02:53 -0500

    Aesthetic changes to page layout

--- flaskapp/views.py
+++ flaskapp/views.py
@@ -61,40 +61,8 @@ def gdata(inputdate=None):
     global data    
     return json.dumps(data)
    
-#    inputdate_code = Date2Code(request.args.get('inputdate'))
-#    
-#        
-#    # find topics
-#    min_clust_size = 3
-#    topics = findTopics(inputdate_code, min_clust_size=min_clust_size)
-#    
-#    # format data
-#    ntopics = len(topics['titles'])
-#    A = topics['clustersizes']
-#    kw = [' | '.join(t) for t in topics['keywords']]
-#    summ = ['<br />'.join(t) for t in topics['titles']]
-#    headlines = topics['titles']
-#
-#    colscale = ["#e5e5e5","#e5d5cc","#e5c5b2","#e5b599","#e5a47f",
-#                "#e59466","#e5844c","#e57433"]+["#e56419"]*1000
-#    col = [colscale[i - min_clust_size] for i in A]
-#   
-#    return json.dumps([{"area": A[i], 
-#                        "color": col[i], 
-#                        "keywords": kw[i],
-#                        "summary": summ[i],
-#                        "headlines": headlines[i]} 
-#                      for i in range(ntopics)])
-#        
-
-#if __name__ == "__main__":
-#    import os
-#
-#    # Open a web browser pointing at the app.
-#    port = 8000
-#
-#    os.system("open http://localhost:{0}/".format(port))
-#
-#    # Set up the development server on port 8000.
-#    app.debug = True
-#    app.run(port=port)
+@app.route('/about/')
+#@app.route('/gdata/<string:inputdate>')
+def display_about():    
+    return "Coming very soon!"
+   
